845420fa04ca41aa6815438f057455937cde7e30
zerotume <zerotume@gmail.com>
2023-02-28 14:30:01 -0500

    merge everything together and change the file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { CssBaseline, Grid } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
 
-import { getPlacesData } from './api/travelAdvisorAPI';
-import Header from './components/Header/Header';
-import List from './components/List/List';
-import Map from './components/Map/Map';
+import { getPlacesData } from "./api/travelAdvisorAPI";
+import Header from "./components/Header/Header.jsx";
+import List from "./components/List/List.jsx";
+import Map from "./components/Map/Map.jsx";
 
 const App = () => {
-  const [type, setType] = useState('restaurants');
-  const [rating, setRating] = useState('');
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
 
   const [coords, setCoords] = useState({});
   const [bounds, setBounds] = useState(null);
@@ -21,9 +21,11 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
-      setCoords({ lat: latitude, lng: longitude });
-    });
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoords({ lat: latitude, lng: longitude });
+      }
+    );
   }, []);
 
   useEffect(() => {
@@ -36,13 +38,12 @@ const App = () => {
     if (bounds) {
       setIsLoading(true);
 
-      getPlacesData(type, bounds.sw, bounds.ne)
-        .then((data) => {
-          setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
-          setFilteredPlaces([]);
-          setRating('');
-          setIsLoading(false);
-        });
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setRating("");
+        setIsLoading(false);
+      });
     }
   }, [bounds, type]);
 
@@ -59,7 +60,7 @@ const App = () => {
     <>
       <CssBaseline />
       <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
-      <Grid container spacing={3} style={{ width: '100%' }}>
+      <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
           <List
             isLoading={isLoading}
@@ -71,7 +72,16 @@ const App = () => {
             setRating={setRating}
           />
         </Grid>
-        <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Map
             setChildClicked={setChildClicked}
             setBounds={setBounds}
